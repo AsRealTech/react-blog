4d5e78b8267d2df802daa84c3219a989bffd1ba5
AsRealTech <114587693+AsRealTech@users.noreply.github.com>
2025-06-08 20:02:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import { useState, useEffect } from 'react';
-import ListComponent from './ListComponent';
+import DataFetcher from './DataFetcher';
 
 function App() {
-  const [userData, setUserData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await resp.json();
-        setUserData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <>
       <h1>User List</h1>
-      <ListComponent userLists={userData} />
+      <DataFetcher />
     </>
   );
 }
